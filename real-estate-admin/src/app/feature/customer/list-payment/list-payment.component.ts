e56c793864a868e2d46626c8ed60d10aff44e89f
Bui Hai Minh <buihaiminh.2k@gmail.com>
2023-06-09 20:14:09 +0700

    fix payment, balance

--- real-estate-admin/src/app/feature/customer/list-payment/list-payment.component.ts
+++ real-estate-admin/src/app/feature/customer/list-payment/list-payment.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {PaymentConfirm} from "../model/payment-confirm";
 import {Router} from "@angular/router";
 import {CustomerService} from "../service/customer.service";
+import {MessageService} from "primeng/api";
 
 @Component({
   selector: 'app-list-payment',
@@ -9,7 +10,6 @@ import {CustomerService} from "../service/customer.service";
   styleUrls: ['./list-payment.component.scss']
 })
 export class ListPaymentComponent implements OnInit {
-  mockCustomerData: PaymentConfirm[];
   paymentData = [];
   depositReq = {
     userName: '',
@@ -18,51 +18,58 @@ export class ListPaymentComponent implements OnInit {
     page: 1,
     pageSize: 10
   };
+  paymentRequest = {
+    customerId: '',
+    amountWallet: '',
+    amountWalletPromotional: '',
+    currency: ''
+  };
+  customerName = '';
   totalRecord = 0;
   pageSize = 10;
   dateTime = [];
   page = 1;
+
   constructor(
     private router: Router,
-    private customerService: CustomerService
+    private customerService: CustomerService,
+    private messageService: MessageService,
   ) {
   }
 
+  isPayment = false;
+
   ngOnInit(): void {
-    this.mockCustomerData = [
-      {
-        id: "KH001",
-        customerName: "John Doe",
-        phoneNumber: "123-456-7890",
-        proofImage: "https://example.com/image1.jpg",
-        status: "Approved",
-        timestamp: "2023-06-01 10:30 AM",
-      },
-      {
-        id: "KH002",
-        customerName: "Jane Smith",
-        phoneNumber: "987-654-3210",
-        proofImage: "https://example.com/image2.jpg",
-        status: "Pending",
-        timestamp: "2023-06-02 02:45 PM",
-      },
-      // Thêm các dữ liệu khách hàng khác tại đây nếu cần
-    ];
     this.doGetListDeposit();
   }
+
   doGetListDeposit() {
     this.customerService.getDepositRequest(this.depositReq).subscribe(res => {
-      console.log(res)
       this.paymentData = res.data;
       this.totalRecord = res.totalCount;
     })
   }
-   paginate(evt: any) {
+
+  paginate(evt: any) {
     if (this.depositReq.pageSize !== evt.rows) {
       this.page = 0;
     }
     this.depositReq.page = evt.page + 1;
     this.depositReq.pageSize = evt.rows;
     this.doGetListDeposit();
+  }
+
+  openPayment(customer: any) {
+    this.paymentRequest.customerId = customer.id;
+    this.paymentRequest.currency = 'VND';
+    this.customerName = customer.customerName;
+    this.isPayment = true;
+  }
+
+  confirmRequest() {
+    this.customerService.paymentRequest(this.paymentRequest).subscribe(res => {
+      this.isPayment = false;
+      this.messageService.add({severity: 'success', detail: 'Thao tác thành công '})
+    })
   }
 }
--- real-estate-admin/src/app/feature/customer/service/customer.service.ts
+++ real-estate-admin/src/app/feature/customer/service/customer.service.ts
@@ -21,24 +21,34 @@ export class CustomerService extends BaseService {
   }
 
   getCustomer(body: any): Observable<any> {
-    return  this.doPost('customer/search', body).pipe(map((res: any) => res));
+    return this.doPost('customer/search', body).pipe(map((res: any) => res));
   }
 
-  getDetailCustomer(id: any): Observable<any>  {
+  getDetailCustomer(id: any): Observable<any> {
     return this.doGet(`customer/id?Id=${id}`).pipe(map((res: any) => res));
   }
 
-  updateCustomer(body: any): Observable<any>  {
-   return this.doPut('customer/update', body).pipe(map((res: any) => res));
+  updateCustomer(body: any): Observable<any> {
+    return this.doPut('customer/update', body).pipe(map((res: any) => res));
+  }
+
+  deposit(body: any): Observable<any> {
+    return this.doPut('depositRequest', body).pipe(map((res: any) => res));
   }
 
   deleteCustomer(body: any): Observable<any> {
     return this.doPost('customer/delete', body).pipe(map((res: any) => res));
   }
-  getBalance(body: any): Observable<any>{
+
+  getBalance(body: any): Observable<any> {
     return this.doPost('/gw/Walllet/wallet/searchTransaction', body).pipe(map((res: any) => res));
   }
-  getDepositRequest(body: any): Observable<any>{
-    return  this.doPost('/depositRequest/ViewAdmin', body).pipe(map((res: any) => res));
+
+  getDepositRequest(body: any): Observable<any> {
+    return this.doPost('/depositRequest/ViewAdmin', body).pipe(map((res: any) => res));
+  }
+
+  paymentRequest(body: any): Observable<any> {
+    return this.doPut('wallet/updateAmount', body).pipe(map((res: any) => res));
   }
 }
