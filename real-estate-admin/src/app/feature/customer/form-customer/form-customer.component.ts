f4635552fb76d083f9e6a524d1cc752468c463c2
DESKTOP-VCHS5NN\Bui Hai Minh <trung18f1@gmail.com>
2023-06-28 00:08:34 +0700

    customer form

--- real-estate-admin/src/app/feature/customer/form-customer/form-customer.component.ts
+++ real-estate-admin/src/app/feature/customer/form-customer/form-customer.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TranslateService} from "@ngx-translate/core";
+import {forkJoin} from "rxjs";
 
 @Component({
   selector: 'app-form-customer',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-customer.component.scss']
 })
 export class FormCustomerComponent implements OnInit {
+  listTypicalBroker: any[] = [];
+  listStatus: any[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private translateService: TranslateService,
+  ) {
   }
 
+  ngOnInit(): void {
+    this.initData();
+  }
+
+  initData() {
+    forkJoin([
+      this.translateService.get('createCustomers.status'),
+      this.translateService.get('createCustomers.typicalBroker')
+    ]).subscribe(res => {
+      console.log(res)
+      this.listStatus = res[0];
+      this.listTypicalBroker = res[1];
+    })
+  }
 }
