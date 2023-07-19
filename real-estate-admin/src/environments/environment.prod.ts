93aaec049690565fc68986adbbc3726b16c8c659
DESKTOP-KJNF2QE\Admin <vantu2102@gmail.com>
2023-07-19 17:08:37 +0700

    .

--- real-estate-admin/src/environments/environment.prod.ts
+++ real-estate-admin/src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  tokenEx: 43199,
+  refreshTokenEx: 43199,
+  clientId: 'YnJvd3NlcjoxMjM0',
+  baseUrl: 'https://api.hoalac123.xyz/',
+  basePath: '',
+  logServer: false,
+  logClient: true,
+  version: '1.0.0',
+  mediaUrl: 'https://cdn.eztek.net/',
+  clientTimeout: 10000, // 10s
+  importExportTimeout: 300000, // 5m
+  idleTimeout: 1800
 };
